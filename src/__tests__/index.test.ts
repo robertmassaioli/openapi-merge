fc435b6e1fa381320d08cbef16f442e7b38bcc6a
Robert Massaioli <rmassaioli@atlassian.com>
2020-05-12 08:42:29 +1000

    Fixed the testing to perform code coverage.

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,6 +1,5 @@
 import 'jest';
 import { merge } from '..';
-import { SingleMergeInput } from '../data';
 import { expectErrorType, expectMergeResult, toMergeInputs } from './test-utils';
 import { toOAS } from './oas-generation';
 
