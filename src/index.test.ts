5e79ade1bbe6a074f0fdf3ca8a2ce173ff8e97c5
Robert Massaioli <rmassaioli@atlassian.com>
2020-05-11 16:41:29 +1000

    Wrote a test case for the extension logic.

--- src/index.test.ts
+++ src/index.test.ts
@@ -567,4 +567,39 @@ describe('merge', () => {
       expectErrorType(merge([firstInput, secondInput]), 'duplicate-paths');
     });
   });
+
+  describe('extensions', () => {
+    it('should take the first extension definition at the top level', () => {
+      const first = toOAS({});
+      first["x-atlassian-narrative"] = {
+        documents: [{
+          anchor: 'first-intro',
+          title: 'First Introduction',
+          body: 'First intro section for reading'
+        }]
+      };
+
+      const second = toOAS({});
+      second["x-atlassian-narrative"] = {
+        documents: [{
+          anchor: 'second-intro',
+          title: 'Second Introduction',
+          body: 'Second intro section for reading'
+        }]
+      };
+
+      const output = toOAS({});
+      output["x-atlassian-narrative"] = {
+        documents: [{
+          anchor: 'first-intro',
+          title: 'First Introduction',
+          body: 'First intro section for reading'
+        }]
+      };
+
+      expectMergeResult(merge(toMergeInputs([first, second])), {
+        output
+      });
+    });
+  });
 });
