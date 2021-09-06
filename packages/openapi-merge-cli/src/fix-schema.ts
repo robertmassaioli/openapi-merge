dfe0f083c40b54e658aaebfe4a109578118fcb01
Robert Massaioli <rmassaioli@atlassian.com>
2021-09-06 14:13:10 +1000

    Making it so that we can generate the docs again.

--- packages/openapi-merge-cli/src/fix-schema.ts
+++ packages/openapi-merge-cli/src/fix-schema.ts
@@ -5,6 +5,7 @@ const inputFile = path.join('src', 'configuration.schema.json');
 
 const schema = JSON.parse(fs.readFileSync(inputFile).toString());
 
+schema.$id = "https://github.com/robertmassaioli/openapi-merge/blob/main/packages/openapi-merge-cli/src/data.ts";
 schema.title = "Configuration";
 schema.description = "The Configuration file for the OpenAPI Merge CLI Tool.";
 
